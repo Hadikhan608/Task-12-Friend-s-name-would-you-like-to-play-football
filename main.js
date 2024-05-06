36da240ec769e8b9a12ffdcb44fbad04ae73089d
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 23:10:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Task 12: 
+let Names = ["Kumail", "Shahid", "Tahami"];
+let message = "Do you like to play football";
+console.log(Names[0] + " " + message);
+console.log(Names[1] + " " + message);
+console.log(Names[2] + " " + message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Task 12: print friend's name one by one in an array with a message inviting them to play football. 
+let Names:string[] = ["Kumail","Shahid","Tahami"];
+let message: string ="Do you like to play football";
+console.log(Names[0] +" "+ message);
+console.log(Names[1] +" "+ message);
+console.log(Names[2] +" "+ message);
